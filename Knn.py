586ff481096478d3059e06e62feb47477d9d2acc
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-03-13 20:16:57 -0600

    Se agregaron comentarios explicando las lineas de codigo

--- Knn.py
+++ Knn.py
@@ -14,27 +14,26 @@ def distanciaEuclidiana(distancia1: list, distancia2: list):
 # prediccion que son los datos con los que vamos a buscar el resultado
 
 def kVecinos (datos, k, prediccion):
-    elementos_juntos = list(zip(*datos.values()))
-    distancias = list()
-    conteo = {}
-    #print(list(c))
-    for indice, elemento in enumerate(elementos_juntos):
-        resultado= distanciaEuclidiana([elemento[0], elemento[1]], prediccion)
-        distancias.append({indice: resultado})
-    distancias= sorted(distancias, key= lambda x: list(x.values())[0])
+    elementos_juntos = list(zip(*datos.values())) #Juntamos los valores en las listas que contienen los diccionarios y al resultado lo convertimos a una lista de listas
+    distancias = list() #Generamos una lista vacia donde guardaremos las distancias euclidianas
+    conteo = {} #Generamos un diccionario vacio donde vamos a almacenar los resultados que se tengan
 
-    for elemento in distancias[:k]:
-        indice = next(iter(elemento.keys()))
-        resultado = elementos_juntos[indice][-1]
-        print(resultado)
+    for indice, elemento in enumerate(elementos_juntos): #Iteramos a la variable elementos juntos obtenemos su índice y su valor
+        resultado= distanciaEuclidiana([elemento[0], elemento[1]], prediccion) #Guardaremos el resultado de la distancia euclidiana
+        distancias.append({indice: resultado}) #Guardamos en la lista de distancias el resultado que salío donde su clave es el índice en el que estamos iterando
+    distancias= sorted(distancias, key= lambda x: list(x.values())[0]) #Una vez finaliza el ciclo ordenamos la lista por medio de los valores que este tenga
 
-        if resultado in conteo:
+    for elemento in distancias[:k]: #iteramos cada elemento de la lista distancias que va desde el elemento 0 hasta el número del elemento que queremos obtener
+        indice = next(iter(elemento.keys())) #Obtenemos la clave del elemento que estamos iterando
+        resultado = elementos_juntos[indice][-1] #Obtenemos el resultado que tiene esa posición accediendo al último elemento de la misma lista
+
+        if resultado in conteo: #Si la clave ya existe en el diccionario conteo sumaremos uno al valor que este actualmente
             conteo[resultado]= conteo[resultado] + 1
-        else:
+        else: #De lo contrario guardamos en el diccionario el valor e inicializamos en uno ese valor
             conteo[resultado]= 1
 
-    prediccion_final = max(conteo, key=conteo.get)
-    print(f"\nLa predicción es: {prediccion_final}")
+    prediccion_final = max(conteo, key=conteo.get) #Obtenemeos el valor máximo del diccionario y obtendremos la clave que este contiene
+    print(f"\nLa predicción es: {prediccion_final}") #Imprimimos en consola el resultado de la predicción
 
 entrada:dict = {
     "horas":       (10, 15, 18, 20, 25, 30),
